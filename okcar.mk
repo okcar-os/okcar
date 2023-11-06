ebb07396d539b5e08f4256c32483bc04a0c12188
Leo <leohuangyi@foxmail.com>
2023-11-07 03:25:24 +0800

    Add ueventd.okcar.rc.

--- okcar.mk
+++ okcar.mk
@@ -16,7 +16,8 @@ PRODUCT_COPY_FILES += \
     okcar/init/init.okcar.rc:vendor/etc/init/hw/init.okcar.rc \
     okcar/init/init.exynos9820.okcar.rc:vendor/etc/init/hw/init.exynos9820.okcar.rc \
     okcar/init/init.qcom.okcar.rc:vendor/etc/init/hw/init.qcom.okcar.rc \
-    okcar/init/okcar.hidreport.bin:vendor/etc/init/hw/okcar.hidreport.bin
+    okcar/init/okcar.hidreport.bin:vendor/etc/init/hw/okcar.hidreport.bin \
+    okcar/init/ueventd.okcar.rc:vendor/etc/init/hw/ueventd.okcar.rc
 
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     okcar/build/target/product/security/recovery/okcarprod
