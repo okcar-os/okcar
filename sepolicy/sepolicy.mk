95bd9ab0f4d89736c4f514021abe1c43faec7ac2
Leo <leohuangyi@foxmail.com>
2024-01-27 23:44:37 +0800

    Fix missing vendor_sysfs_usb_supply.

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,18 +1,14 @@
 #$(error "=============TARGET_BOARD_PLATFORM:$(TARGET_BOARD_PLATFORM)")
 ifneq (,$(filter sdm845 sdm710 msm8998, $(TARGET_BOARD_PLATFORM)))
     BOARD_VENDOR_SEPOLICY_DIRS += \
-        okcar/sepolicy/vendor_sysfs_usb
+        okcar/sepolicy/vendor_sysfs_usb \
+        okcar/sepolicy/sysfs_usb_supply_2_vendor_sysfs_usb_supply
 endif
 
 ifneq (,$(filter universal9820, $(TARGET_BOARD_PLATFORM)))
     BOARD_VENDOR_SEPOLICY_DIRS += \
         okcar/sepolicy/vendor_sysfs_usb \
         okcar/sepolicy/universal9820
-endif
-
-ifneq (,$(filter mt6771, $(TARGET_BOARD_PLATFORM)))
-    BOARD_VENDOR_SEPOLICY_DIRS += \
-        okcar/sepolicy/vendor_sysfs_usb
 endif
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
